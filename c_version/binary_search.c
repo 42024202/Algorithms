c916aeb3498fa33b974b3df9e73ee6e6a4e75e5e
baiemir <baemir.omurzak0v@mail.ru>
2026-03-12 22:41:15 +0600

    Create package for version for 'C'

---
+++ c_version/binary_search.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int binary_search(int arr[], int target)
+{
+    int low, high, mid;
+    high = sizeof(arr)/sizeof(int);
+    low = 0;
+
+    for(low = 0, low <= high; mid = (low + high) / 2)
+    {
+        
+    }
+}
