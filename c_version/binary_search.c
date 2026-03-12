8891fefde15122004fbe2c337f8402f267ff6940
baiemir <baemir.omurzak0v@mail.ru>
2026-03-13 00:05:24 +0600

    Binary_search function on C is ready

--- c_version/binary_search.c
+++ c_version/binary_search.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 
+int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-int binary_search(int arr[], int target)
+void binary_search(int *arr, int target, int size)
 {
-    int low, high, mid;
-    high = sizeof(arr)/sizeof(int);
+    int low, high, mid, guess;
+    high = size - 1;
     low = 0;
 
-    for(low = 0, low <= high; mid = (low + high) / 2)
+    while (low <= high)
     {
-        
+        mid = (low + high) / 2;
+        guess = arr[mid];
+        if (guess == target)
+        {
+            printf("Your number is %d", guess);
+            break;
+        }
+        else if (guess > target)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
     }
 }
+
+int main()
+{
+    binary_search(numbers, 2, 8);
+}
+
